4ffb0ab1598102d0a133a81c10bc2c73cfa7a17b
4ce <88108711+4cecoder@users.noreply.github.com>
2022-01-08 14:23:12 -0500

    Update upd.sh

--- upd.sh
+++ upd.sh
@@ -1,3 +1,3 @@
-#update ubuntu
-
-sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
+#!/bin/sh
+#ubuntu 
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean
