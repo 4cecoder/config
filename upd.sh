0c8c47f928c69cfa3bb1cebb8b3e3ccc3ac88b6e
4ce <88108711+4cecoder@users.noreply.github.com>
2022-01-08 14:25:25 -0500

    Update upd.sh

--- upd.sh
+++ upd.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-#ubuntu or debian
+
+
+
+#detect ubuntu or debian run this
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean
+
+#detect if macos then brew stuff
+#brew upgrade
+#arch -arm64 brew upgrade 
