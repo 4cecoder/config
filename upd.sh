b7d970b93d46d3e0eee4e3d8db58b5634f1ce8a7
4ce <88108711+4cecoder@users.noreply.github.com>
2022-01-08 14:28:12 -0500

    Update upd.sh

--- upd.sh
+++ upd.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+#gentoo
+#emaint -a sync && emerge --sync && emerge -uU --deep --newuse @world && emerge -c
 
+#archlin 
+#sudo pacman -Syu
 
 #detect ubuntu or debian run this
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean
