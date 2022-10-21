aa98367848da576f174b9f1957b22d0e9d0cf018
4ce <88108711+4cecoder@users.noreply.github.com>
2022-10-21 16:24:05 -0400

    Update gupd.sh
    optional syncs

--- gupd.sh
+++ gupd.sh
@@ -3,4 +3,4 @@
 # gentoo 
 
 # repo update & pull latest code merge then build and clean
-emaint -a sync && emerge-webrsync && emerge -uU --deep --newuse @world && emerge -avc
+emaint -a sync && emerge-webrsync -a && emerge -uU --deep --newuse @world && emerge -avc
